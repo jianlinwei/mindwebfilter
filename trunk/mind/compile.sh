33dab5d5e3b9a31b5ab4efc28e62ed879d95f1f7
mind.gnu@gmail.com <mind.gnu@gmail.com@8482dc1d-ec65-669a-5cfd-07a2471d507b>
2011-02-28 20:42:15 +0000

    Compile script added (debug mode)

---
+++ trunk/mind/compile.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ "$USER" != "root" ]
+then
+  echo "This script must be executed as root"
+  exit 1
+fi
+
+if [ $# -ne 2 ]
+then
+  echo "Usage: $0 proxyuser proxygroup"
+  echo "Example: $0 proxy proxy"
+  exit 2
+fi
+
+make distclean
+./autogen.sh&&./configure --mandir=/usr/share/man/ --enable-clamd=yes --with-proxyuser=$1 \
+--with-proxygroup=$2 --prefix=/ --mandir=/usr/share/man \
+--infodir=/usr/share/info --sysconfdir=/etc --localstatedir=/var \
+--enable-icap=yes --enable-commandline=yes --enable-trickledm=yes --enable-email=yes \
+--enable-ntlm=yes CXX="g++" CXXFLAGS="-O0 -g" LDFLAGS="-Wl,-Bsymbolic-functions" \
+CPPFLAGS="" CC="cc" CFLAGS="-O0 -g"&&make
+
